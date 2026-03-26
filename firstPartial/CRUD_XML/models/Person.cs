172ca9a879ac5d899057ed2c19d539b26d389ff4
DanCrux404 <jorgedantex@gmail.com>
2026-03-26 12:31:16 -0600

    Update Person model

--- firstPartial/CRUD_XML/models/Person.cs
+++ firstPartial/CRUD_XML/models/Person.cs
@@ -5,7 +5,6 @@ public class Person
     public string? Name { get; set; }
     public int Age { get; set; }
     public string? Email { get; set; }
-
     public Person(){}
 
     public Person(string? name, int age, string? email)
